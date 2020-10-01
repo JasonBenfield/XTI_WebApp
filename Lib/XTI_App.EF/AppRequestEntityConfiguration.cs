7d27f58633d89c889f7af1b609d6e18dc2e26ed5
Jason Benfield <jbenfield108@gmail.com>
2020-09-30 22:38:02 -0400

    use entity type configurations (#45)

---
+++ Lib/XTI_App.EF/AppRequestEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace XTI_App.EF
+{
+    public sealed class AppRequestEntityConfiguration : IEntityTypeConfiguration<AppRequestRecord>
+    {
+        public void Configure(EntityTypeBuilder<AppRequestRecord> builder)
+        {
+            builder.HasKey(r => r.ID);
+            builder.Property(r => r.ID).ValueGeneratedOnAdd();
+            builder.Property(s => s.Path).HasMaxLength(100);
+            builder
+                .HasOne<AppSessionRecord>()
+                .WithMany()
+                .HasForeignKey(s => s.SessionID);
+            builder
+                .HasOne<AppVersionRecord>()
+                .WithMany()
+                .HasForeignKey(s => s.VersionID);
+        }
+    }
+}
