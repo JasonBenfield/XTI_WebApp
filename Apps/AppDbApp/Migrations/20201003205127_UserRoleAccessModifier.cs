370fef2011bd7878d6072202116e52a43992f4fc
Jason Benfield <jbenfield108@gmail.com>
2020-10-03 17:16:48 -0400

    added db migrations for user role access modifier

---
+++ Apps/AppDbApp/Migrations/20201003205127_UserRoleAccessModifier.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfMigrationsApp.Migrations
+{
+    public partial class UserRoleAccessModifier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Modifier",
+                table: "UserRoles",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Modifier",
+                table: "UserRoles");
+        }
+    }
+}
