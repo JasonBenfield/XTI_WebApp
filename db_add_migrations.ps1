00d71f156a3fea57e7dab863e52cf4c607b85c77
Jason Benfield <jbenfield108@gmail.com>
2020-10-04 19:00:36 -0400

    publish appdbapp after adding migrations

--- db_add_migrations.ps1
+++ db_add_migrations.ps1
@@ -1,3 +1,9 @@
 param ([Parameter(Mandatory)]$name)
 $env:ASPNETCORE_ENVIRONMENT="Development"
+$env:DOTNET_ENVIRONMENT="Development"
 dotnet ef migrations add $name --project ./Apps/AppDbApp
+
+$currentDir = (Get-Item .).FullName
+Set-Location Apps/AppDbApp
+dotnet publish  /p:PublishProfile=Local
+Set-Location $currentDir
